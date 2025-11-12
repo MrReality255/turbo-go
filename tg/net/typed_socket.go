5db1b0ee5b05603f9ff57f8b5c8b0ea4c6f1c231
MrReality <martin.mojzis.acem@gmail.com>
2025-11-12 07:57:32 +0100

    Typed sockets refactored

---
+++ tg/net/typed_socket.go
@@ -0,0 +1,119 @@
+package net
+
+import (
+	"io"
+	"sync"
+
+	"github.com/MrReality255/turbo-go/tg/log"
+)
+
+type IAbstractSocket interface {
+	io.Closer
+	Read(b []byte) (n int, err error)
+	Write(b []byte) (n int, err error)
+}
+
+type ITypedSocket[T any] interface {
+	io.Closer
+	Read() (T, error)
+	Write(data T) error
+}
+
+type TypedSocket[T any] struct {
+	conn     io.Closer
+	mx       *sync.Mutex
+	isClosed bool
+
+	onRead  func() (T, error)
+	onWrite func(data T) error
+}
+
+type TypedSocketFactory[T any] struct {
+	onRead  func(conn IAbstractSocket) (T, error)
+	onWrite func(conn IAbstractSocket, data T) error
+}
+
+func NewTypedSocketFactory[T any](
+	onRead func(conn IAbstractSocket) (T, error),
+	onWrite func(conn IAbstractSocket, data T) error,
+) *TypedSocketFactory[T] {
+	return &TypedSocketFactory[T]{
+		onRead:  onRead,
+		onWrite: onWrite,
+	}
+}
+
+func (tsf *TypedSocketFactory[T]) NewTcpClient(addr string, port int) (ITypedSocket[T], error) {
+	c, err := NewTcpClient(addr, port)
+	if err != nil {
+		return nil, err
+	}
+	return tsf.New(c), nil
+}
+
+func (tsf *TypedSocketFactory[T]) New(src IAbstractSocket) ITypedSocket[T] {
+	return &TypedSocket[T]{
+		mx:   &sync.Mutex{},
+		conn: src,
+		onRead: func() (T, error) {
+			return tsf.onRead(src)
+		},
+		onWrite: func(data T) error {
+			return tsf.onWrite(src, data)
+		},
+	}
+}
+
+func (m *TypedSocket[T]) abort(hint string, err error) {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	if m.isClosed {
+		return
+	}
+
+	log.LogError(hint, err)
+	if err = m.closeLocked(); err != nil {
+		log.LogError("error while closing the socket: %v", err)
+	}
+}
+
+func (m *TypedSocket[T]) closeLocked() error {
+	if m.isClosed {
+		return nil
+	}
+	m.isClosed = true
+	return m.conn.Close()
+}
+
+func (m *TypedSocket[T]) Close() error {
+	m.mx.Lock()
+	defer m.mx.Unlock()
+	return m.closeLocked()
+}
+
+func (m *TypedSocket[T]) Read() (T, error) {
+	return m.onRead()
+}
+
+func (m *TypedSocket[T]) RequestHandlerLoop(requestHandler func(msg T) (T, error)) {
+	for {
+		msg, err := m.Read()
+		if err != nil {
+			m.abort("error while reading the message", err)
+		}
+
+		response, err := requestHandler(msg)
+		if err != nil {
+			m.abort("error while handling the request", err)
+			return
+		}
+
+		if err = m.Write(response); err != nil {
+			m.abort("error while writing the response", err)
+		}
+	}
+}
+
+func (m *TypedSocket[T]) Write(data T) error {
+	return m.onWrite(data)
+}
---
+++ tg/net/utils.go
@@ -0,0 +1,71 @@
+package net
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+)
+
+func GetServerAddr(port int) string {
+	return fmt.Sprintf("0.0.0.0:%d", port)
+}
+
+func ServeLocal(port int, handler func(conn net.Conn) error, errHandler func(error)) error {
+	return Serve(GetServerAddr(port), handler, errHandler)
+}
+func Serve(addr string, handler func(conn net.Conn) error, errHandler func(err error)) error {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			return err
+		}
+
+		go func() {
+			err := CloseAfter(conn, func() error {
+				return handler(conn)
+			})
+			if err != nil && errHandler != nil {
+				errHandler(err)
+			}
+		}()
+	}
+}
+
+func NewTcpClient(addr string, port int) (net.Conn, error) {
+	c, err := net.Dial("tcp", fmt.Sprintf("%v:%v", addr, port))
+	return c, err
+}
+
+func NewTcpServer(port int, handler func(c net.Conn), errHandler func(error)) (io.Closer, error) {
+	listener, err := net.Listen("tcp4", GetServerAddr(port))
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for {
+			c, err := listener.Accept()
+			if err != nil && errHandler != nil {
+				errHandler(err)
+			}
+			handler(c)
+		}
+
+	}()
+	return listener, nil
+}
+
+func GetHttpFrom(url string, target interface{}) error {
+	var cl http.Client
+	r, err := cl.Get(url)
+	if err != nil {
+		return err
+	}
+	return FromJSON(r.Body, target)
+}
